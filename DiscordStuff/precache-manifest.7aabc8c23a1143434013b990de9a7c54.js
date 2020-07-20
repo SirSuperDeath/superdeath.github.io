0e8d1e5ea5308e8046f2e52b3b89443504895554
SuperDeath <techasmusic@gmail.com>
2020-07-20 19:44:36 +0100

    push

---
+++ DiscordStuff/precache-manifest.7aabc8c23a1143434013b990de9a7c54.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40a1f6f9af7d8a29bd407bbc13b065a3",
+    "url": "/BetterDiscordStuff/index.html"
+  },
+  {
+    "revision": "717ef930666512624693",
+    "url": "/BetterDiscordStuff/static/css/main.1359096b.chunk.css"
+  },
+  {
+    "revision": "f8768f816958eb3413de",
+    "url": "/BetterDiscordStuff/static/js/2.b6fd7b9e.chunk.js"
+  },
+  {
+    "revision": "8ec8b65940d4527c3e1ef85a62fa0e11",
+    "url": "/BetterDiscordStuff/static/js/2.b6fd7b9e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "717ef930666512624693",
+    "url": "/BetterDiscordStuff/static/js/main.8549102d.chunk.js"
+  },
+  {
+    "revision": "761c453297bd818950be",
+    "url": "/BetterDiscordStuff/static/js/runtime-main.67e26751.js"
+  }
+]);
